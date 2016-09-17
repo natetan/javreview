1758574cafb8fb5bc59702f51bd5e8834adbcfbe
yulongtan <swifties@uw.edu>
2016-09-16 21:27:58 -0700

    initial test

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Yulong on 9/16/2016.
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        PracticeArray array = new PracticeArray();
+        for (int i = 0; i < 5; i++) {
+            array.add(i);
+        }
+        System.out.println(array);
+    }
+}
