fd631a49bce5514128c1ec6d291a53a54e98fbde
yulongtan <swifties@uw.edu>
2016-09-17 16:47:10 -0700

    tested remove

--- src/Test.java
+++ src/Test.java
@@ -8,6 +8,12 @@ public class Test {
         for (int i = 0; i < 5; i++) {
             array.add(i);
         }
-        System.out.println(array);
+        System.out.println("Initial array: " + array.toString());
+        array.remove(array.size() - 1); // removes 4
+        System.out.println("Remove at end: " + array.toString()); // [0, 1, 2, 3]
+        array.remove(0); // removes 0
+        System.out.println("Remove at front: " + array.toString()); // [1, 2, 3]
+        array.remove(1); 
+        System.out.println("Removed first index: " + array.toString()); // [1, 3]
     }
 }
